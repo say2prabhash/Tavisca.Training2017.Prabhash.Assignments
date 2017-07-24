1282764c0a43d815e35eef18b9d942cc9a47d2d0
say2prabhash <pmohanty@tavisca.com>
2017-07-24 23:41:00 +0530

    MarsRover File Added

---
+++ Tavisca.Training2017.Handson.MarsRover/MarsRoverTest/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
+using MarsRover;
+
+namespace MarsRoverTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [Fact]
+        public void Rover_is_at_the_left_corner()
+        {
+            RoverMovement rover = new RoverMovement();
+                Xunit.Assert.Equal(true,rover.Rover_is_moving_backward(0,0));
+           
+        }
+    }
+}
